db056596b3b22d10a3febf1774a11d548343174c
Kevin Siwicke <kevin.siwicke@noaa.gov>
2023-05-25 13:09:47 -0800

    added a wrapper function

---
+++ R/whale_dep.R
@@ -0,0 +1,34 @@
+#' wrapper function to produce plots
+#'
+#' @param channel where the at-sea Access database is
+#' @param station station number
+#'
+#' @return plots for viewing
+#' @export whale_dep
+#'
+#' @examples
+#' \dontrun{
+#' whale_dep(channel, station)
+#' }
+whale_dep <- function(channel, station) {
+  names <- RODBC::sqlQuery(channel , base::paste0("select * from \"Sppcode Catch\"")) %>%
+    dplyr::rename_all(tolower)
+
+  strat <- RODBC::sqlQuery(channel , base::paste0("select * from DepthStratum")) %>%
+    dplyr::rename_all(tolower)
+
+  dat <- get_data(channel = channel, sta_num = station)
+  depth = dat[[1]]
+  cat = dat[[2]]
+
+  plot_dat <- make_plot_data(depth = depth, cat = cat, strat = strat, names = names)
+
+  depth_strat <- plot_dat[[1]]
+  depth_strat$effective <- 45 - depth_strat$ineffective
+  spp_sum_strat <- plot_dat[[2]]
+  dep <- plot_dat[[3]]
+  catch <- plot_dat[[4]]
+  spp_sum <- plot_dat[[5]]
+
+  plt <- plot_data(depth_strat = depth_strat, spp_sum_strat = spp_sum_strat, dep = dep, catch = catch)
+}
